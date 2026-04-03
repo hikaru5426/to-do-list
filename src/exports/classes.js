a9c613709743b4a957deb0bb3d403839a417a2e7
Hikaru <122536382+hikaru5426@users.noreply.github.com>
2026-04-03 16:58:11 +0200

    Continued to add methods to ProjectManager and TaskManager, among others, can now change the project of a task, and visually list all projects and the tasks they contain in the console

--- src/exports/classes.js
+++ src/exports/classes.js
@@ -1,7 +1,7 @@
 class Task {
     static nextId = 0;
 
-    constructor(title, projectTitle, description = "", dueDate = null, priority = 0){
+    constructor(title, projectTitle, description = "", dueDate = null, priority = 0) {
         this.id = Task.nextId++;
         this.title = title;
         this.projectTitle = projectTitle;
@@ -13,7 +13,7 @@ class Task {
 
 class Project {
 
-    constructor(title){
+    constructor(title) {
         this.title = title;
         this.tasks = [];
     }
@@ -23,28 +23,32 @@ class Project {
 class TaskManager {
     static tasks = [];
 
-    static findTaskWithId(taskId){
-        return TaskManager.tasks.find(task => task.id === taskId);
+    static findTaskWithId(taskId) {
+        return this.tasks.find(task => task.id === taskId);
     }
 
-    static addTask(title, projectTitle, description = "", dueDate = null, priority = 0){
+    static addTask(title, projectTitle, description = "", dueDate = null, priority = 0) {
         let newTask = new Task(title, projectTitle, description, dueDate, priority);
-        TaskManager.tasks.push(newTask);
+        this.tasks.push(newTask);
         const project = ProjectManager.findProject(projectTitle);
         project.tasks.push(newTask);
     }
 
-    static removeTask(oldTaskId){
-        const oldTask = TaskManager.findTaskWithId(oldTaskId);
+    static removeTask(oldTaskId) {
+        const oldTask = this.findTaskWithId(oldTaskId);
         const project = ProjectManager.findProject(oldTask.projectTitle);
-        project.tasks.filter(task => task.id !== oldTaskId);
-        TaskManager.tasks.filter(item => item.id !== id);
+        project.tasks = project.tasks.filter(task => task.id !== oldTaskId);
+        this.tasks = this.tasks.filter(item => item.id !== id);
     }
 
-    static editTask(taskId, title, projectTitle, description, dueDate, priority){
+    static editTask(taskId, title, oldProjectTitle, description, dueDate, priority, newProjectTitle = null) {
         const task = this.findTaskWithId(taskId);
         task.title = title;
-        task.projectTitle = projectTitle;
+
+        if (newProjectTitle) {
+            ProjectManager.changeProjectOfTask(taskId, oldProjectTitle, newProjectTitle);
+        }
+
         task.description = description;
         task.dueDate = new Date(dueDate[0], dueDate[1] - 1, dueDate[2]);
         task.priority = priority;
@@ -66,33 +70,52 @@ class TaskManager {
 class ProjectManager {
     static projects = [];
 
-    static addProject(title){
+    static addProject(title) {
         let project = new Project(title);
-        ProjectManager.projects.push(project);
+        this.projects.push(project);
     }
 
-    static removeProject(title){
-        ProjectManager.filter(project => project.title !== title);
+    static removeProject(title) {
+        this.projects = this.projects.filter(project => project.title !== title);
     }
 
-    static findProject(title){
-        return ProjectManager.projects.find(project => project.title === title);
+    static findProject(title) {
+        return this.projects.find(project => project.title === title);
     }
 
-    static editProject(title){
+    static editProject(title) {
         const project = findProject(title);
         project.title = title;
     }
 
-    static isTitleAvailable(title){
-        for(const project of ProjectManager.projects){
-            if(project.title === title){
+    static changeProjectOfTask(taskId, oldProjectTitle, newProjectTitle) {
+        const oldProject = this.findProject(oldProjectTitle);
+        oldProject.tasks = oldProject.tasks.filter(task => task.id !== taskId);
+
+        const newProject = this.findProject(newProjectTitle);
+        const task = TaskManager.findTaskWithId(taskId);
+        newProject.tasks.push(task);
+        task.projectTitle = newProject.title;
+    }
+
+    static isTitleAvailable(title) {
+        for (const project of this.projects) {
+            if (project.title === title) {
                 return false;
             }
         }
         return true;
     }
 
+    static listProjectsAndTasks(){ // For dev and debug purpose
+        this.projects.forEach(function(project){
+            console.log(`Projet : ${project.title} :`);
+            console.log(`ID : ${project.id}`);
+            console.log(`Tasks :`);
+            project.tasks.forEach(task => console.log(`ID : ${task.id}, title : ${task.title}, projectTitle : ${task.projectTitle}, description : ${task.description}, dueDate : ${task.duteDate}, priority : ${task.priority}`));
+            console.log("");
+        })
+    }
 }
 
-export {Task, Project, TaskManager, ProjectManager};
+export { Task, Project, TaskManager, ProjectManager };
--- src/index.js
+++ src/index.js
@@ -5,4 +5,7 @@ import { format, addDays, subDays, addMonths, differenceInDays, isBefore, isAfte
 window.debug = {TaskManager, Task, ProjectManager, Project};
 
 ProjectManager.addProject("Default Project");
+ProjectManager.addProject("Project Number 2");
 TaskManager.addTask("Sortir le chien", "Default Project", "le chien a besoin de faire sa promenade journalière", [2026, 3, 17], 2);
+TaskManager.addTask("Sortir le chat", "Default Project", "le chat a besoin de sortir tous les jours", [2025, 5, 2], 3);
+TaskManager.addTask("Manger sainement", "Project Number 2", "Il est important pour la santé de bien manger", [2027, 6, 11], 1);
